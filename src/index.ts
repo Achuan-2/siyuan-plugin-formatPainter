e306ff393bed822e9704f321d5e6ff62531fcc3a
Tron <achuan-2@outlook.com>
2024-10-10 13:11:42 +0800

    :tada:first commit:

---
+++ src/index.ts
@@ -0,0 +1,180 @@
+import {
+    Plugin,
+    getFrontend,
+    getBackend,
+    fetchPost,
+    IModel,
+    Protyle,
+    IProtyle,
+    Toolbar
+} from "siyuan";
+import "./index.scss";
+
+const STORAGE_NAME = "menu-config";
+const TAB_TYPE = "custom_tab";
+const DOCK_TYPE = "dock_tab";
+
+export default class PluginSample extends Plugin {
+    private customTab: () => IModel;
+    private isMobile: boolean;
+    private formatPainterEnable = false;
+    private formatData: { datatype: string, style: string } | null = null;
+    private protyle: IProtyle;
+    onload() {
+        console.log(this.app);
+        this.data[STORAGE_NAME] = { readonlyText: "Readonly" };
+        this.protyleOptions = {
+            toolbar: ["block-ref",
+                "a",
+                "|",
+                "text",
+                "strong",
+                "em",
+                "u",
+                "s",
+                "mark",
+                "sup",
+                "sub",
+                "clear",
+                "|",
+                "code",
+                "kbd",
+                "tag",
+                "inline-math",
+                "inline-memo",
+                "|",
+                {
+                    name: "format-painter",
+                    icon: "iconFormat",
+                    tipPosition: "n",
+                    tip: this.i18n.tips,
+                    click: (protyle: Protyle) => {
+                        this.protyle = protyle.protyle;
+                        
+                        if (!this.formatPainterEnable) {
+                            const selectedInfo = this.getSelectedParentHtml();
+                            if (selectedInfo) {
+                                this.formatData = {
+                                    datatype: selectedInfo.datatype,
+                                    style: selectedInfo.style
+                                };
+                                this.formatPainterEnable = true;
+                                // console.log(this.formatData);
+                                fetchPost("/api/notification/pushMsg", { "msg": this.i18n.enable, "timeout": 7000 });
+                            }
+
+                        }
+                    }
+                }
+            ],
+        };
+        // this.eventBus.on("click-editorcontent", ({ detail }) => {
+        //     if (this.formatPainterEnable && this.formatData) {
+        //         this.protyle = detail.protyle;
+        //     }
+
+        // });
+        document.addEventListener('mouseup', (event) => {
+            if (this.formatPainterEnable && this.formatData) {
+                const selection = window.getSelection();
+                if (selection && selection.rangeCount > 0) {
+                    const range = selection.getRangeAt(0);
+                    const selectedText = range.toString();
+                    if (selectedText) {
+                        this.protyle.toolbar.range = range;  // 更改选区
+                        // console.log(this.protyle.toolbar.range.toString());
+                        // Apply the stored format to the selected text
+                        if (this.formatData.datatype) {
+                            this.protyle.toolbar.setInlineMark(this.protyle, this.formatData.datatype, "range");
+                        }
+                        if (this.formatData.style) {
+                            console.log(this.formatData.style);
+                            // this.protyle.toolbar.setInlineMark(this.protyle, "text", "range", { "type": "style1", "color": this.formatData.style });
+                            const { backgroundColor, color } = parseStyle(this.formatData.style);
+
+                            if (backgroundColor) {
+                                this.protyle.toolbar.setInlineMark(this.protyle, "text", "range", {
+                                    "type": "backgroundColor",
+                                    "color": backgroundColor
+                                });
+                            }
+
+                            if (color) {
+                                this.protyle.toolbar.setInlineMark(this.protyle, "text", "range", {
+                                    "type": "color",
+                                    "color": color
+                                });
+                            }
+                        }
+                        // console.log("Format applied to selected text");
+                        // 清空选区
+                        selection.removeAllRanges();
+                    }
+                }
+            }
+        });
+        function parseStyle(styleString) {
+            const styles = styleString.split(';').filter(s => s.trim() !== '');
+            const styleObject = {};
+
+            styles.forEach(style => {
+                const [property, value] = style.split(':').map(s => s.trim());
+                styleObject[property] = value;
+            });
+
+            return {
+                backgroundColor: styleObject['background-color'],
+                color: styleObject['color']
+            };
+        }
+        document.addEventListener('keydown', (event) => {
+            if (event.key === 'Escape') {
+                this.formatPainterEnable = false;
+                this.formatData = null;
+                fetchPost("/api/notification/pushMsg", { "msg": this.i18n.disable, "timeout": 7000 });
+            }
+        });
+
+        console.log(this.i18n.helloPlugin);
+    }
+
+    getSelectedParentHtml() {
+        const selection = window.getSelection();
+        if (selection.rangeCount > 0) {
+            const range = selection.getRangeAt(0);
+            const selectedNode = range.startContainer;
+            const startNode = selectedNode.nodeType === Node.TEXT_NODE ? selectedNode.parentNode : selectedNode;
+            let parentElement = selectedNode.parentElement;
+            while (parentElement && !parentElement.hasAttribute("data-type")) {
+                parentElement = parentElement.parentElement;
+            }
+            if (parentElement) {
+                const result = {
+                    html: parentElement.outerHTML,
+                    datatype: parentElement.getAttribute("data-type"),
+                    style: parentElement.getAttribute("style")
+                };
+                // 清空选区
+                selection.removeAllRanges();
+                return result;
+            }
+
+        }
+        // 清空选区
+        selection.removeAllRanges();
+        return null;
+    }
+
+    onLayoutReady() {
+        this.loadData(STORAGE_NAME);
+        console.log(`frontend: ${getFrontend()}; backend: ${getBackend()}`);
+    }
+
+    onunload() {
+        console.log(this.i18n.byePlugin);
+    }
+
+    uninstall() {
+        console.log("uninstall");
+    }
+}
